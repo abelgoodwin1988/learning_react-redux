c154ae7605cc10c1c2e60d5d76ee793743033af5
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2017-09-14 19:19:39 -0600

    step 59

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
-    render() {
-      return (
-        <form className="input-group">
-          <input />
-          <span className="input-group-btn">
-            <button type="button" className="btn btn-secondary">Button</button>
-          </span>
-        </form>
-      );
-    }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: ""
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+    this.setState({term:event.target.value})
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onFormSubmit} className="input-group">
+        <input
+          placeholder="Get a five-day forecast in your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+        <span className="input-group-btn">
+          <button type="sumbit" className="btn btn-secondary">Button</button>
+        </span>
+      </form>
+    );
+  }
 }
