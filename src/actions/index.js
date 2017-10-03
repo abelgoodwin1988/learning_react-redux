0234fce86bf04d3eddf6868b9e404aebe12bcc90
Abel Goodwin <abelgoodwin1988@gmail.com>
2017-10-02 20:48:15 -0600

    weather reducer scaffolding

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = 'e31fafbcad2e40b05747342fbda620d6';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
-  const url = `${ROOT_URL}&q=${city},us`
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
 
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   };
 }
